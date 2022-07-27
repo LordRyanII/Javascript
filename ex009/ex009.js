c5e6b100e2881c81eeb11ab518b22fae78fa77c2
LordRyanII <ryanoliveiram2020@outlook.com>
2022-07-26 22:02:15 -0300

    Aprendendo condições compostas com if() e else

---
+++ ex009/ex009.js
@@ -0,0 +1,7 @@
+var país = 'França'
+console.log(`Vivendo em ${país}`)//exemplo de condição composta
+if(país == 'Brasil'){
+    console.log('Você é Brasileiro!')
+}else{
+    console.log('Você é estrangeiro!')
+}
