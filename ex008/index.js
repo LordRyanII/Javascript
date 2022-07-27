048eb1e2b33c6b342b94dcb3c53e25fe42064dbe
LordRyanII <ryanoliveiram2020@outlook.com>
2022-07-26 21:40:59 -0300

    Trabalhando condições if()

---
+++ ex008/index.js
@@ -0,0 +1,7 @@
+console.log('O arquivo funcionou corretamente!')
+
+var velocidade = 70
+if(velocidade > 60){ //condição simples, quando o resultado da TRUE
+    console.log(`VocÊ ultrapassou o limite de velocidade. MULTADO!. sua velocidade atual é ${velocidade}km/h!`)
+}
+console.log('Use sempre cinto de segurança!')
