f006d907a4d22eaa240704c28ea580f02b2bf7c3
LordRyanII <ryanoliveiram2020@outlook.com>
2022-08-14 22:49:40 -0300

    Aprendendo condições aninhadas
    Aprendendo mais sobre lógica com if e else.

---
+++ ex011/ex011.js
@@ -0,0 +1,10 @@
+//exercicio javascript condições aninhadas
+
+var idade = 67
+if (idade < 16) {
+    console.log('Não vota')
+}else if(idade < 18 || idade > 65){
+        console.log('Voto opcional')
+}else{
+    console.log('voto obrigatório')
+}
