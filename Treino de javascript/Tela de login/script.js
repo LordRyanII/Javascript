aab3ab266c084ba827070fcc0bd148435793f91d
LordRyanII <ryanoliveiram2020@outlook.com>
2022-08-18 22:01:21 -0300

    Praticando com arrays
    Aprendendo e praticando, manipulações com arrays.

---
+++ Treino de javascript/Tela de login/script.js
@@ -0,0 +1,19 @@
+//começo do meu código
+
+
+function clicar(){
+    console.log('ola')
+    var email = window.document.getElementById('#e-mail');
+    var password = window.document.getElementById('#senha');
+    var array = [
+        'ryanoliveiram2015@gamil.com',
+        'renata.gremio123@outlook.com',
+        'adriel.devolops@yahoo.com',
+    ];
+    //condições
+    if(email === null){
+        window.alert('e-mail incorreto')
+    } else if(email === array){
+        window.alert(`você confirma esse e-mail ${email}` )
+    }
+}
